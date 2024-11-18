2927f46a56d7cdcff95bb18e2a4ff163fd16c212
Asshley <asshleydeanwaynegozar@gmail.com>
2024-11-18 16:50:04 +0800

    Created Deposit and Transaction Function

--- Main.py
+++ Main.py
@@ -12,6 +12,7 @@ def load_accounts(file_path = "Database\\accounts.json"):
         accounts = []
         print("Error loading file, Starting with empty list")
     return accounts
+    
 
 accounts = load_accounts()
 
@@ -19,8 +20,9 @@ def create_account(account):
     try:
         account_number = int(input("Enter your account number: "))
         pin = int(input("Create your pin(4 digits only!): "))
+        deposit = int(input("Please enter initial deposit: "))
 
-        info = {"account-number":account_number, "pin":pin}
+        info = {"account-number":account_number, "pin":pin, "balance": deposit}
         account.append(info)
 
         try:
@@ -31,6 +33,63 @@ def create_account(account):
             print("File not found!")
     except ValueError:
         print("Integer Only!")
-    
+    log_in(accounts)
+
+def log_in(accounts):
+    try:
+        account_number = int(input("Enter your account number: "))
+        pin = int(input("Enter your pin: "))
+
+        for account in accounts:
+            if account["account-number"] == account_number and account["pin"] == pin:
+                print("Log In Successful!")
+                transactions()
+            else:
+                print("Invalid Account Number or Pin")
+
+    except ValueError:
+        print("Integer Input Only!")
+
+def transactions():
+    while True:
+        print("Choose your Transaction: ")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Check Balance")
+        print("4. Exit")
+        try:
+            choice = int(input("Enter here: "))
+            match choice:
+                case 1:
+                    deposit(accounts)
+                    break
+                case 2:
+                    pass
+                case 3:
+                    pass
+                case 4:
+                    break
+                case _:
+                    print("Invalid Input!")
+        except ValueError:
+            print("Integer Only!")
+def deposit(accounts):
+    try:
+        amount = int(input("Enter amount to deposit here: "))
+        account_number = int(input("Enter your account number: "))
+        pin = int(input("Enter your pin for confirmation: "))
+
+        for account in accounts:
+            if account["account-number"] == account_number and account["pin"] == pin:
+               total_amount = account["balance"] + amount
+               account["balance"] = total_amount
+               try:
+                   with open(file_path, "w") as file:
+                       json.dump(accounts,file, indent=3)
+               except FileNotFoundError:
+                   print("File not Found!")
+               print("Deposit Successful!")
+    except ValueError:
+        print("Integer Only!")
 
 create_account(accounts)
